a686d04e8ac9d102a6a23ea9deee654807276ad7
maryorimontoya111 <maryorimontoya1113908@correo.itm.edu.co>
2024-11-29 14:28:42 -0500

    Cambio 2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hola mundo, este mensaje es para el profe")
+    print("Hola mundo, este mensaje es para el profe de Aplicación y Servicios Web")
  
 if __name__ == "__main__":
     main()
